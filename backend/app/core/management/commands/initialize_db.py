c12d91f5af1b678d9dba29c14bfd06fa1012ee8a
Breno Tostes <tostes@pop-os.localdomain>
2023-10-23 00:46:36 -0300

    Added test for db initialized with data from invest.com

--- backend/app/core/management/commands/initialize_db.py
+++ backend/app/core/management/commands/initialize_db.py
@@ -2,7 +2,7 @@
 Django command to initialize the db with most recent B3 values.
 """
 import yahooquery as yq
-from b3symbols import symbols
+import investpy as inv
 
 import pandas as pd
 
@@ -30,6 +30,7 @@ class Command(BaseCommand):
 
     def __get_all_tickers(self):
         """Uses Yahoo Query to fetch ticker information."""
+        symbols = self.__get_symbols()
 
         # YahooQuery utilizes the '.SA' termination for B3 assets
         tickers = []
@@ -44,4 +45,18 @@ class Command(BaseCommand):
         df = df.reset_index(level=['symbol', 'date'])
         df = df[['symbol', 'date', 'close']].groupby('symbol').tail(1)
         df['date'] = df['date'].map(lambda x: pd.to_datetime(x))
+        df = df.rename(columns={
+            'symbol': 'symbol',
+            'date': 'date',
+            'close': 'value'
+        })
         return df
+
+    def __get_symbols(self):
+        """Get symbols from investing.com"""
+        br_symbols = inv.stocks.get_stocks(country='brazil')['symbol']
+        br_symbols = list(filter(
+            lambda x: ('11' not in x) and (len(x) <= 5),
+            br_symbols
+        ))
+        return br_symbols
