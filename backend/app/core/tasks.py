5be51c27b8713da53897f01b2880bbf85096c793
Breno Tostes <tostes@pop-os.localdomain>
2023-12-01 03:06:13 -0300

    Implemented shared task for checking tunnels' thresholds.

--- backend/app/core/tasks.py
+++ backend/app/core/tasks.py
@@ -1,6 +1,13 @@
+"""
+Shared tasks for celery beat.
+"""
+
 from celery import shared_task
 from celery.utils.log import get_task_logger
+
+from django.utils import timezone
 from django.core.management import call_command
+from models import Tunnel
 
 
 logger = get_task_logger(__name__)
@@ -16,5 +23,20 @@ def sample_task():
 def update_db_task():
     """Task for updating Assets on db."""
 
-    # Evokes
+    # Evokes django admin command for updating db
     call_command('initialize_db')
+
+
+@shared_task
+def check_tunnel_thresholds_task():
+    """Task for checking if current ticker value crosses thresholds"""
+    now = timezone.now()
+
+    for tunnel in Tunnel.objects.all():
+        if (now - tunnel.lastChecked) >= tunnel.interval:
+            if tunnel.assetId.value > tunnel.upperVal:
+                pass  # TODO send email
+            if tunnel.assetId.value < tunnel.lowerVal:
+                pass  # TODO send email
+            tunnel.lastChecked = now
+            tunnel.save()
